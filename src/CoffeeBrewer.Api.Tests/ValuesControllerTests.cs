137716ee6b14d37f3e2b0549e790b1e417a29b58
Steven Trigg <steventriggnz@gmail.com>
2024-08-10 15:31:12 +1200

    test cicd failing test

--- src/CoffeeBrewer.Api.Tests/ValuesControllerTests.cs
+++ src/CoffeeBrewer.Api.Tests/ValuesControllerTests.cs
@@ -24,5 +24,7 @@ public class ValuesControllerTests
         Assert.Equal("[\"value1\",\"value2\"]", response.Body);
         Assert.True(response.Headers.ContainsKey("Content-Type"));
         Assert.Equal("application/json; charset=utf-8", response.Headers.First(x => x.Key == "Content-Type").Value);
+
+        Assert.False(true);
     }
 }
