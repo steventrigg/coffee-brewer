3b11b689d82de6938d7b271ccaf2b380e6380fe9
Steven Trigg <steventriggnz@gmail.com>
2024-08-12 15:09:21 +1200

    fix validation behaviour

--- src/CoffeeBrewer.App/ValidationBehaviour.cs
+++ src/CoffeeBrewer.App/ValidationBehaviour.cs
@@ -11,13 +11,13 @@ namespace CoffeeBrewer.App
             _validators = validators;
         }
 
-        public Task<IResult> Handle(TRequest request, RequestHandlerDelegate<IResult> next, CancellationToken cancellationToken)
+        public async Task<IResult> Handle(TRequest request, RequestHandlerDelegate<IResult> next, CancellationToken cancellationToken)
         {
             var validators = _validators.Where(x => x is IValidator<TRequest>);
 
             foreach (var validator in _validators)
             {
-                var ex = validator.ValidateAsync(request);
+                var ex = await validator.ValidateAsync(request);
                 if (ex != null)
                 {
                     // Short-circuit. A more ideal alternative would be to check all validators and return a list.
@@ -25,14 +25,14 @@ namespace CoffeeBrewer.App
 
                     if (result != null)
                     {
-                        return Task.FromResult(result);
+                        return result;
                     }
 
                     throw new Exception("Instance activation failed");
                 }
             }
 
-            return next();
+            return await next();
         }
     }
 }
