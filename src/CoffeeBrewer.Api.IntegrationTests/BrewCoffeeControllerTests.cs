46104da5c958e760612c53e59e81283b061a8b48
Steven Trigg <steventriggnz@gmail.com>
2024-08-12 18:25:59 +1200

    fixed up tests

--- src/CoffeeBrewer.Api.IntegrationTests/BrewCoffeeControllerTests.cs
+++ src/CoffeeBrewer.Api.IntegrationTests/BrewCoffeeControllerTests.cs
@@ -5,14 +5,21 @@ using Amazon.Lambda.APIGatewayEvents;
 
 namespace CoffeeBrewer.Api.Tests;
 
-public class ValuesControllerTests
+public class BrewCoffeeControllerTests
 {
     [Fact]
-    public async Task TestGet()
+    public async Task Test_BrewCoffee()
     {
+        var now = DateTime.Now;
+        if (now.Month == 4 && now.Day == 1)
+        {
+            // Don't fail on april fools
+            Assert.True(true);
+        }
+
         var lambdaFunction = new LambdaEntryPoint();
 
-        var requestStr = File.ReadAllText("./SampleRequests/ValuesController-Get.json");
+        var requestStr = File.ReadAllText("./SampleRequests/BrewCoffeeController-Get.json");
         var request = JsonSerializer.Deserialize<APIGatewayHttpApiV2ProxyRequest>(requestStr, new JsonSerializerOptions
         {
             PropertyNameCaseInsensitive = true
@@ -21,7 +28,6 @@ public class ValuesControllerTests
         var response = await lambdaFunction.FunctionHandlerAsync(request, context);
 
         Assert.Equal(200, response.StatusCode);
-        Assert.Equal("[\"value1\",\"value2\"]", response.Body);
         Assert.True(response.Headers.ContainsKey("Content-Type"));
         Assert.Equal("application/json; charset=utf-8", response.Headers.First(x => x.Key == "Content-Type").Value);
     }
